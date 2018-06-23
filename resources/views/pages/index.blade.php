7d3c18167f73f457a5d0f81299d61ac4bd2de11a
james-chege <jymeschege@gmail.com>
2018-06-23 07:39:27 +0300

    setup the app name

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,11 +5,13 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>{{config('app.name', 'Nice')}}</title>
 
 
     </head>
     <body>
+    <h1>This is the index page</h1>
+    <p>Gland to have reached you!</p>
 
     </body>
 </html>
