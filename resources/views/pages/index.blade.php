bbb96a3b8c988be091ee582cad46e10b76b50652
james-chege <jymeschege@gmail.com>
2018-06-25 12:19:22 +0300

    add slide show

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -13,8 +13,38 @@
 @section('image')
 <div class="row">
     <div class="col-md-12">
-        <div class="topic-image img-responsive thumbnail text-center">
-            <img src="{{asset('imgs/web01.png')}}" alt="Portfolio Image" />
+        <div id="slideshow" class="topic-image img-responsive thumbnail text-center">
+            <div class="slideshow-container">
+
+                <div class="mySlides">
+                    <img src="{{asset('imgs/web01.png')}}" alt="Portfolio Image" />
+                </div>
+                <div class="mySlides">
+                    <img src="{{asset('imgs/inner-banner-joinus_0.jpg')}}" alt="Portfolio Image" />
+                </div>
+                <div class="mySlides">
+                    <img src="{{asset('imgs/inner-banner-psd.jpg')}}" alt="Portfolio Image" />
+                </div>
+                <div class="mySlides">
+                    <img src="{{asset('imgs/inner-banner-contact.jpg')}}" alt="Portfolio Image" />
+                </div>
+                <div class="mySlides">
+                    <img src="{{asset('imgs/inner-banner-portfolio2.jpg')}}" alt="Portfolio Image" />
+                </div>  
+                </br>
+
+                <!-- Next and previous buttons -->
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+                <!-- The dots/circles -->
+                <div style="text-align:center">
+                    <span class="dot" onclick="currentSlide(1)"></span> 
+                    <span class="dot" onclick="currentSlide(2)"></span> 
+                    <span class="dot" onclick="currentSlide(3)"></span> 
+                </div>
+                
+            </div>
         </div>
     </div>    
 </div>
