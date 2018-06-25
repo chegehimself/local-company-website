c6a55cc496e6db1de764c78b51cad6c0e5361678
james-chege <jymeschege@gmail.com>
2018-06-26 00:06:41 +0300

    modify about

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -16,3 +16,85 @@
     <h1><strong>WHO WE ARE</strong><h1/>
 </div>
 @endsection
+
+@section('clients')
+<div class="container">
+<h2>Orangesoft:</h2>
+<h2>Helping your company grow into the Digital Age</h2>
+<h5><b>Did you know, your website is the new store front for your business in the Digital Age?</b></h5>
+
+Here's the secret of a successful website: don't create a website for the sake of having one....create a website that helps you generate profits for your business!
+
+Truth is, without a professional website, you'll lose customers, market share and erode your brand. This means your competition is taking over your market.
+
+That's why you need a professional partner for your website, someone with a proven track record that helps your business get results.
+<div id="we">
+<div id="about-we" class="row">
+    <div class="col-md-6 text-center">
+           <h4 class="make-me-bold"> <strong> One of the best Web Design companies worldwide </strong> </h4>
+           <img class="aboutus-image" src="{{asset('imgs/aboutus01.png')}}" alt="about image">
+           <p class="text-left">
+                Orangesoft has helped over 500 companies, government agencies and SMEs worldwide in the past 10 years. We have an excellent reputation as one of Malaysia's leading Web design companies.
+
+                The fact is, we possess a strong track record of delivering on-time and to the expectations of our clients.
+                
+                Whether you want a fancy website with amazing functionalities, or just a simple website for branding purposes...Orangesoft has a solution for you.
+                
+                Solutions that help you stand above and beyond your competition.
+                
+                Imagine having a large group of award-winning Web designers and programmers working for you. You can rest assured that you will get the best website; 
+                one that fits your business vision. Not just a fancy-looking one.
+           </p>
+    </div>
+    <div class="col-md-6 text-center">
+            <h4 class="make-me-bold text-center"> <strong> Customisation is our Specialty</strong></h4>
+            <img class="aboutus-image" src="{{asset('imgs/aboutus02.png')}}" alt="about image">
+            <p class="text-left">
+                    The truth is no two clients are alike. You have your own unique business objectives and vision. That's why we believe in customizing your website to support the pursuit of your business goals.
+
+                    Our philosophy is simple, your website should be a vehicle that accelerates the achievement of your business goals. Unlike others, we are not interested to sell standard packages that limit your potential.
+                    
+                    And best of all, with Orangesoft, your only limitation is your imagination.
+            </p>
+    </div>
+</div>
+
+<div id="about-we" class="row">
+        <div class="col-md-6 text-center">
+               <h4 class="make-me-bold text-center"> <strong> Scalability is our Priority </strong> </h4>
+               <img class="aboutus-image" src="{{asset('imgs/aboutus01.png')}}" alt="about image">
+               <p class="text-left">
+                    Business in today's modern world is not static. It's dynamic and changing all the time. To succeed, you need a website that grows with your business and adapts to changes in the rapidly evolving business environment.
+
+                    That's why we specialize in building websites on the Drupal platfrom; the very same platform used by the largest corporations and government agencies in the world. Websites such as The White House, FedEx, Linkedin and Stanford University use this powerful and versatile platform.
+                    
+                    What does this mean for you?
+                    
+                    It means your Website can grow with your organisation, without the need to rebuild it each time your organisation grows and evolves.
+                    
+                    To put it simply, Orangesoft is staffed with an elite group of experts and specialists equipped to do medium and big projects, that's why we are your best choice to fulfill both your present and future business needs.
+        </div>
+        <div class="col-md-6 text-center">
+                <h4 class="make-me-bold"> <strong> Our TOP services include:</strong></h4>
+                <img class="aboutus-image" src="{{asset('imgs/aboutus02.png')}}" alt="about image">
+                <p class="text-left">
+                        Custom-made web design and conceptualization
+Web integration with Drupal content management system
+Portal development (for real estate agencies, social community portal, custom portal among others)
+E-catalog
+E-commerce
+Web marketing (Search engine optimization, search engine marketing, social media marketing, etc.)
+Orangesoft simply transforms your Website from a cost centre into a profit centre. A profit centre 
+that supports the pursuit of your business's objectives.
+
+You can start profiting from your website and make it pay for itself, or you can waste time, money 
+and get frustrated.
+
+Contact the professionals today for a friendly chat, you'll be glad you did.
+                </p>
+        </div>
+    </div>
+</div>
+</div>
+
+@endsection
