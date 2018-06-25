497315c73ec99595b8a9f37fa45f7af6c2943b54
james-chege <jymeschege@gmail.com>
2018-06-25 07:44:33 +0300

    fix nav logo

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $(window).on('scroll', function() {
 
     if (scrollTop >= 150 && scrollTop <= 400) {
         $('#top-top').stop().animate({padding: "0px 0px 0px 0px"}, 800);
-        $('#nav-log').stop().animate({height: "50px"}, 800);
+        // $('#nav-log').stop().animate({height: "50px"}, 800);
         $('#navigation-main').stop().animate({height: "50px"}, 800);
         $('#navigation-main').stop().animate({padding: "10px 0px 0px 0px"}, 800);
     } else {
